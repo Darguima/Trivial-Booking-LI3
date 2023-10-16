82ce236bc2f0cd5b5c6992ab723551f3dc4d06dd
Dário Guimarães <dsgdevbraga@gmail.com>
2023-10-16 20:42:17 +0100

    New Makefile that can receive params

--- trabalho-pratico/src/main.c
+++ trabalho-pratico/src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+int main(int argc, char **argv) {
+  if (argc == 3) {
+    printf("Entered in Batch Mode with params '%s' & '%s'.\n", argv[1], argv[2]);
+  } else if (argc == 1) {
+    printf("Entered in Interactive Mode\n");
+  } else {
+    printf("Call the program with 2 params (dataset & queries file) to batch mode.\n");
+    printf("Call the program without params to interactive mode.\n");
+    fprintf(stderr, "[ERROR] Invalid params\n");
+    return 1;
+  }
 
-
-
-int main () {
-
-    printf("Hello World!\n");
-    return 0;
+  return 0;
 }
