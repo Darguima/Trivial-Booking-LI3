f55ed6bbf9dbd4bfd40ef90259cf5abb2af29786
Dário Guimarães <dsgdevbraga@gmail.com>
2023-11-06 15:47:14 +0000

    Users Schema

--- trabalho-pratico/include/datatypes/datatypes.h
+++ trabalho-pratico/include/datatypes/datatypes.h
@@ -1,3 +1,31 @@
+typedef enum {
+  PLACEHOLDER
+} sex;
+
+typedef enum {
+  PLACEHOLDER
+} pay_method ;
+
+typedef enum {
+  PLACEHOLDER
+} account_status ;
+
+typedef struct usersSchema
+{
+  int id;
+  char *name;
+  char *email;
+  int phone_number; // verify if the size of an int is sufficient for phone number
+  int birth_date; // verify if dates are stored as numeric timestamp
+  sex sex;
+  int passport;
+  int country_code;
+  char *address;
+  int account_creation; // verify how should the date be stored
+  pay_method pay_method;
+  account_status account_status;
+} UsersSchema;
+
 typedef struct flightsSchema
 {
   int id;
@@ -12,8 +40,3 @@ typedef struct reservationsSchema
 {
   int id;
 } ReservationsSchema;
-
-typedef struct usersSchema
-{
-  int id;
-} UsersSchema;
