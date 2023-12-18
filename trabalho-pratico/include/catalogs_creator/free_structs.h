4adceff7f62a3094a2a15deb017e2faeaf91e0df
Dário Guimarães <dsgdevbraga@gmail.com>
2023-12-18 18:24:13 +0000

    Remove some unused files

--- trabalho-pratico/include/catalogs_creator/free_structs.h
+++
@@ -1,9 +0,0 @@
-//! @file
-#include <glib.h>
-
-/*
- * @brief This function will free the memory allocated for the passenger struct.
- *
- * @param value The passenger struct.
- */
-void free_passengers_struct(gpointer value);
